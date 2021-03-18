53dee5eff1a4accd7c7cafe6aa37a3554a99f3ca
Jarno Rantaharju <rantahar@gmail.com>
2021-03-18 17:41:56 +0200

    Update generate_examples to support VAE and BEGAN

--- stackable_autoencoder/generate_examples.py
+++ stackable_autoencoder/generate_examples.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from stackable_autoencoder.models import BlockedAutoencoder, Autoencoder
 import stackable_autoencoder.data
+from stackable_autoencoder import models
 
 if len(sys.argv) > 1:
    autoencoder_path = sys.argv[1]
@@ -26,18 +27,28 @@ BATCH_SIZE = 5
 dataset = stackable_autoencoder.data.dataset_from_folder(data_path, IMG_SIZE, BATCH_SIZE)
 images = next(iter(dataset.take(1)))
 
-autoencoder = Autoencoder(save_path = autoencoder_path, load = True)
+autoencoder = models.Autoencoder(save_path = autoencoder_path, load = True)
 reproduced = autoencoder.call(images)
 
 if gan_path is not None:
-    discriminator = tf.keras.models.load_model(gan_path+"/discriminator")
-    generator = tf.keras.models.load_model(gan_path+"/generator")
+    if(os.path.isdir(gan_path+"/discriminator")):
+        discriminator = tf.keras.models.load_model(gan_path+"/discriminator")
+        generator = tf.keras.models.load_model(gan_path+"/generator")
+        latent_dim = generator.input_shape[-1]
+        noise = tf.random.uniform([BATCH_SIZE, latent_dim], minval=-1)
+        autoencoded = discriminator(images)
+        generated = generator(noise)
 
-    latent_dim = generator.input_shape[-1]
-
-    noise = tf.random.uniform([BATCH_SIZE, latent_dim], minval=-1)
-    discriminated = discriminator(images)
-    generated = generator(noise)
+    if(os.path.isdir(gan_path+"/encoder")):
+        encoder = tf.keras.models.load_model(gan_path+"/encoder")
+        decoder = tf.keras.models.load_model(gan_path+"/decoder")
+        latent_dim = decoder.input_shape[-1]
+        noise = tf.random.uniform([BATCH_SIZE, latent_dim], minval=-1)
+        mean, logvar = encoder(images)
+        eps = tf.random.normal(shape=mean.shape)
+        z = eps * tf.exp(logvar * .5) + mean
+        autoencoded = decoder(z)
+        generated = decoder(noise)
 
 
 print("autoencoder loss: ", autoencoder.evaluate_batch(images).numpy())
@@ -55,7 +66,7 @@ for i in range(BATCH_SIZE):
     plt.imshow(im)
     if gan_path is not None:
         fig.add_subplot(BATCH_SIZE, n_im, n_im*i+3)
-        im = (discriminated[i] + 1) / 2
+        im = (autoencoded[i] + 1) / 2
         plt.imshow(im)
         fig.add_subplot(BATCH_SIZE, n_im, n_im*i+4)
         im = (generated[i] + 1) / 2

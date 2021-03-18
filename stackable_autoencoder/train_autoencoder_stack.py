1f529d11b0dca64870c0baf4284bbdfd0a3f24e6
Jarno Rantaharju <rantahar@gmail.com>
2021-03-18 17:44:09 +0200

    Remove buckets in autoencoder stack

--- stackable_autoencoder/train_autoencoder_stack.py
+++ stackable_autoencoder/train_autoencoder_stack.py
@@ -11,17 +11,9 @@ latent_dim = 64
 scalings = 3
 samples = 10000
 
-
-if len(sys.argv) > 2 and sys.argv[1] == "remote":
-   remote = True
-   save_every = 5000
-   log_step = 1
-   bucket = sys.argv[1]
-else:
-   remote = False
-   save_every = 500
-   log_step = 1
-   bucket = None
+remote = False
+save_every = 500
+log_step = 1
 
 
 
@@ -35,5 +27,5 @@ autoencoder = BlockedAutoencoder(IMG_SIZE, size, encoding_size, latent_dim,
 #train_dataset = train_dataset.take(100)
 #valid_dataset = valid_dataset.take(10)
 print_log()
-autoencoder.train(train_dataset, valid_dataset, epochs, bucket = bucket, log_step = log_step, save_every = save_every)
-autoencoder.save(bucket = bucket)
+autoencoder.train(train_dataset, valid_dataset, epochs, log_step = log_step, save_every = save_every)
+autoencoder.save()
